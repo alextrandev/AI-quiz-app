a53d9bd9f8c4546b7fe194a023704aef5ae45850
Alex Tran <tranduclong092@gmail.com>
2024-09-05 17:47:24 +0300

    Feat: get topic from url and render quiz question and answer options

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,53 +1,44 @@
-import { capitalize } from "@/app/lib/util";
+"use client";
 
-export default function page() {
+import { capitalize } from "@/app/lib/util";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function TestPage() {
+  // get current quiz topic from url
+  const { id } = useParams<{ id: string }>();
+  const router = useRouter();
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [score, setScore] = useState<number>(0);
+  // track current question number
+  const [currQuestion, setCurrQuestion] = useState<number>(0);
+
+  const handleAnswerClick = (answer: string) => { }
 
   return (
     <main className="w-full min-h-screen p-6 flex flex-col items-center justify-center">
       <h2 className="font-bold text-3xl mb-4 text-blue-500">
-        Topics...
+        {capitalize(id)}
       </h2>
       <h3 className="font-semibold text-2xl mb-3">
-        Question: 1 of x
+        Question: {currQuestion + 1} of {questions.length}
       </h3>
       <h3 className="text-xl mb-4">
-        Question go here...
+        {questions[currQuestion]?.question}
       </h3>
 
       <div className="flex flex-col lg:w-1/3 mb-6">
-        {/* mapping question choice buttons */}
-        <button
-          className="p-4 bg-gray-300 rounded-xl mb-6 min-w[200px] text-black text-lg
+        {questions[currQuestion]?.options.map((option, i) => (
+          <button
+            className="p-4 bg-gray-300 rounded-xl mb-6 min-w[200px] text-black text-lg
            hover:bg-red-400 hover:text-white"
-          key={0}
-        // onClick={() => { }}
-        >
-          Answer go here...
-        </button>
-        <button
-          className="p-4 bg-gray-300 rounded-xl mb-6 min-w[200px] text-black text-lg
-           hover:bg-red-400 hover:text-white"
-          key={0}
-        // onClick={() => { }}
-        >
-          Answer go here...
-        </button>
-        <button
-          className="p-4 bg-gray-300 rounded-xl mb-6 min-w[200px] text-black text-lg
-           hover:bg-red-400 hover:text-white"
-          key={0}
-        // onClick={() => { }}
-        >
-          Answer go here...
-        </button>
-        <button
-          className="p-4 bg-gray-300 rounded-xl mb-6 min-w[200px] text-black text-lg
-           hover:bg-red-400 hover:text-white"
-          key={0}
-        // onClick={() => { }}
-        >
-          Answer go here...
-        </button>
+            key={i}
+            onClick={() => handleAnswerClick(option)}
+          >
+            {option}
+          </button>
+        ))}
       </div>
     </main>
   )
