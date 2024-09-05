303727816668931d1cf9aa47ce00212da5998f53
Alex Tran <tranduclong092@gmail.com>
2024-09-05 16:08:09 +0300

    Feat: create a utility file with quiz topics and a helper function

---
+++ app/lib/util.ts
@@ -0,0 +1,20 @@
+// topics list
+export const firstTopics = [
+  { id: "AI", name: "AI quiz" },
+  { id: "PHP", name: "PHP quiz" },
+  { id: "JS", name: "JavaScript quiz" },
+];
+
+export const secondTopics = [
+  { id: "CSS", name: "CSS quiz" },
+  { id: "HTML", name: "HTML quiz" },
+  { id: "UI", name: "UI Design quiz" },
+];
+
+// function to capitalize first character
+export const capitalize = (str: string) => {
+  str = str?.replace(/%20/g, " ");
+
+  // nullish here to avoid error with empty string
+  return str[0].toUpperCase + str.slice(1) + " Questions" ?? str;
+}
