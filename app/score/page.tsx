447b9cb748352d435ae36a3eb2e667063c34c74e
Alex Tran <tranduclong092@gmail.com>
2024-09-07 15:58:34 +0300

    Feat: display the score and percentage

--- app/score/page.tsx
+++ app/score/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function Score() {
+  const urlParams = useSearchParams();
+  const score = urlParams.get("score");
+
   return (
     <main className="p-4 min-h-screen w-full flex flex-col items-center justify-center">
       <h2 className="text-2xl font-semibold">Score</h2>
       <p className="text-lg text-center mb-4">
-        You got 10 {/* to do... */} out of 10 questions correct
+        You got {score} out of 10 questions correct
       </p>
       <h1 className="font-extrabold text-5xl text-blue-500 mb-3">
-        100%
+        {Number(score) * 10}%
       </h1>
       <Link href="/" className="bg-blue-500 p-4 text-blue-50 rounded">
         Take more test
