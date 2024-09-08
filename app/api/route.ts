569dd102482fc0886f8bdfc8098380e5141808e1
Alex Tran <tranduclong092@gmail.com>
2024-09-08 16:46:23 +0300

    Feat: write a template prompt for openAI using with topic var and the sample questions

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+import sampleQuestion from "@/app/lib/sample.json";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export async function POST(req: NextRequest) {
+  // here is the requested topic
   const { topic } = await req.json();
-  console.log({ topic });
+
+  // template prompt for openAI
+  const prompt = `Generate 10 distinct questions on ${topic} and ensure they are in JSON format containing an id, topic which is ${topic}, a question attribute containing the question, an options array of 3 options, and an answer property. Please ensure that the options array is shuffled to ensure that the answer does not retain a single position.
+    - Please don't make the answers too obvious and lengthy.
+    - Ensure the questions are unique and not repetitive.
+    - The questions should not be too simple but intermediate level.
+    - Return only the JSON object containing the questions.
+    You can use this as a sample: ${JSON.stringify(sampleQuestion)}
+  `;
+
   return NextResponse.json({ message: "Fetch complete" }, { status: 200 });
 }
