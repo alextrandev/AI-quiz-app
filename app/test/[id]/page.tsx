092feebcb2869d8eb1e96d3a4e94e088b8af19eb
Alex Tran <tranduclong092@gmail.com>
2024-09-09 12:10:03 +0300

    Refactor: add a confirm stage before moving on to next question

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -14,6 +14,8 @@ export default function TestPage() {
   // track current question number
   const [currQuestion, setCurrQuestion] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
+  // this state define if user already chosen an answer for current question
+  const [answerChosen, setAnswerChosen] = useState<boolean>(false);
 
   // useCallBack will trigger if the given input (id) changes
   const fetchQuestions = useCallback(async () => {
@@ -37,27 +39,70 @@ export default function TestPage() {
   const handleAnswerClick = (answer: string) => {
     // update score state
     setScore(score => answer === questions[currQuestion].answer ? score + 1 : score);
+    setAnswerChosen(true);
+  }
 
+  const handleNextQuestion = () => {
     // questions count
     if (currQuestion < questions.length - 1) {
       setCurrQuestion(count => count + 1);
+      setAnswerChosen(false);
     } else {
       // if this is the last question, wait for score to update before redicrect to score page
       setTimeout(() => {
         router
           .push("/score?score="
             // add the score as url parameter
-            + (answer === questions[currQuestion].answer ? score + 1 : score)
+            + score
             // add the topic as parameter, skip the "question" part of the string
             + `&topic=${capitalize(id).split(' ')[0]}`);
       }, 100);
     }
-  }
+  };
 
+  // loading screen
   if (loading) {
     return <h3 className='font-semibold text-2xl mb-3'>Loading...</h3>;
   }
 
+  // show the right answer and next question button
+  if (answerChosen) {
+    return (
+      <main className="w-full min-h-screen p-6 flex flex-col items-center justify-center">
+        <h2 className="font-bold text-3xl mb-4 text-blue-500">
+          {capitalize(id)}
+        </h2>
+        <h3 className="font-semibold text-2xl mb-3">
+          Question: {currQuestion + 1} of {questions.length}
+        </h3>
+        <h3 className="text-xl mb-4">
+          {questions[currQuestion]?.question}
+        </h3>
+
+        <div className="flex flex-col lg:w-1/3 mb-6">
+          {questions[currQuestion]?.options.map((option, i) => (
+            <button
+              className="p-4 bg-gray-300 rounded-xl mb-6 min-w[200px] text-black text-lg
+           hover:bg-red-400 hover:text-white"
+              key={i}
+              onClick={() => handleAnswerClick(option)}
+            >
+              {option}
+            </button>
+          ))}
+          <button
+            className="p-4 bg-blue-500 rounded-xl mb-6 min-w[200px] text-white text-lg
+           hover:bg-yellow-500 hover:text-black"
+            onClick={() => handleNextQuestion()}
+          >
+            Next question
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  // show the question and 4 answer options
   return (
     <main className="w-full min-h-screen p-6 flex flex-col items-center justify-center">
       <h2 className="font-bold text-3xl mb-4 text-blue-500">
