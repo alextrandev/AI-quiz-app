73f224558d5c5a69b9d25dc90f58ef0d51f391cf
Alex Tran <tranduclong092@gmail.com>
2024-09-09 13:08:24 +0300

    Feat: update some extra quiz topic

--- app/lib/util.ts
+++ app/lib/util.ts
@@ -1,15 +1,56 @@
 // topics list
-export const category_1 = [
+const programming_1 = [
   { id: "AI", name: "AI quiz" },
   { id: "PHP", name: "PHP quiz" },
   { id: "JS", name: "JavaScript quiz" },
+  { id: "Python", name: "Python quiz" },
 ];
 
-export const category_2 = [
+const programming_2 = [
   { id: "CSS", name: "CSS quiz" },
   { id: "HTML", name: "HTML quiz" },
   { id: "UI", name: "UI Design quiz" },
 ];
+
+const music_1 = [
+  { id: "US UK", name: "US UK music quiz" },
+  { id: "KPOP", name: "Kpop music quiz" },
+  { id: "Classical", name: "Vpop music quiz" },
+]
+
+const science_1 = [
+  { id: "Physics", name: "Physics quiz" },
+  { id: "Chemistry", name: "Chemistry quiz" },
+  { id: "Biology", name: "Biology quiz" },
+];
+
+const history_1 = [
+  { id: "World History", name: "World History quiz" },
+  { id: "US History", name: "U.S. History quiz" },
+  { id: "Ancient History", name: "Ancient History quiz" },
+];
+
+const geography_1 = [
+  { id: "World Geography", name: "World Geography quiz" },
+  { id: "US Geography", name: "U.S. Geography quiz" },
+  { id: "Europe Geography", name: "Europe Geography quiz" },
+  { id: "Capitals", name: "Capital Cities quiz" },
+];
+
+const entertainment_1 = [
+  { id: "Movies", name: "Movies quiz" },
+  { id: "TV Shows", name: "TV Shows quiz" },
+  { id: "Celebrities", name: "Celebrities quiz" },
+];
+
+const literature_1 = [
+  { id: "Classics", name: "Classics Literature quiz" },
+  { id: "Modern", name: "Modern Literature quiz" },
+  { id: "Poetry", name: "Poetry quiz" },
+];
+
+// export all catergories
+export const quizCaterories = [programming_1, programming_2, music_1, science_1, history_1, geography_1, entertainment_1, literature_1];
 
 // function to capitalize first character
 export const capitalize = (str: string) => {
